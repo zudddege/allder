e79cc43a9e19f2b5b4d0917dda86880a33365448
Yuttasat <yuttasatpinjai@hotmail.com>
2021-12-03 14:17:12 +0700

    สร้าง folder

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,15 +1,55 @@
 <html lang="en">
 
 <head>
+    <style data-styles="">
+        ion-icon {
+            visibility: hidden
+        }
+
+        .hydrated {
+            visibility: inherit
+        }
+    </style>
+    <style data-styles="">
+        ion-icon {
+            visibility: hidden
+        }
+
+        .hydrated {
+            visibility: inherit
+        }
+    </style>
+    <style data-styles="">
+        ion-icon {
+            visibility: hidden
+        }
+
+        .hydrated {
+            visibility: inherit
+        }
+    </style>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
     <meta name="Author" content="Spruko Technologies Private Limited">
+    <meta name="Keywords"
+        content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4">
+
+    <!-- Title -->
     <title>Allder Express</title>
+
+    <!-- Favicon -->
     <link rel="icon" href="assets/img/brand/icon.png" type="image/x-icon">
+
+    <!-- Icons css -->
     <link href="assets/css/icons.css" rel="stylesheet">
+
+    <!-- Bootstrap css -->
     <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Internal Data table css -->
     <link href="assets/plugins/datatable/datatables.min.css" rel="stylesheet">
     <link href="assets/plugins/datatable/responsive.dataTables.min.css" rel="stylesheet">
     <link href="assets/plugins/datatable/responsive.bootstrap5.css" rel="stylesheet">
@@ -17,19 +57,194 @@
     <link href="assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="assets/plugins/datatable/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="assets/plugins/select2/css/select2.min.css" rel="stylesheet">
+
+    <!--  Right-sidemenu css -->
     <link href="assets/plugins/sidebar/sidebar.css" rel="stylesheet">
+
+    <!-- P-scroll bar css-->
     <link href="assets/plugins/perfect-scrollbar/p-scrollbar.css" rel="stylesheet">
+
+    <!-- Sidemenu css -->
     <link rel="stylesheet" href="assets/css/sidemenu.css">
+
+    <!--- Style css --->
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/boxed.css" rel="stylesheet">
     <link href="assets/css/dark-boxed.css" rel="stylesheet">
+
+    <!--- Dark-mode css --->
     <link href="assets/css/style-dark.css" rel="stylesheet">
+
+    <!---Skinmodes css-->
     <link href="assets/css/skin-modes.css" rel="stylesheet">
+
+    <!--- Animations css-->
     <link href="assets/css/animate.css" rel="stylesheet">
+
+    <script src="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/ionicons.z18qlu2u.js"
+        data-resources-url="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/" data-namespace="ionicons">
+    </script>
+    <style type="text/css"
+        id="s./node_modules/css-loader/index.js!./node_modules/sass-loader/lib/loader.js!./package/src/animation.scss-0">
+        /**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+        .eva-animation {
+            animation-duration: 1s;
+            animation-fill-mode: both;
+        }
+
+        .eva-infinite {
+            animation-iteration-count: infinite;
+        }
+
+        .eva-icon-shake {
+            animation-name: eva-shake;
+        }
+
+        .eva-icon-zoom {
+            animation-name: eva-zoomIn;
+        }
+
+        .eva-icon-pulse {
+            animation-name: eva-pulse;
+        }
+
+        .eva-icon-flip {
+            animation-name: eva-flipInY;
+        }
+
+        .eva-hover {
+            display: inline-block;
+        }
+
+        .eva-hover:hover .eva-icon-hover-shake,
+        .eva-parent-hover:hover .eva-icon-hover-shake {
+            animation-name: eva-shake;
+        }
+
+        .eva-hover:hover .eva-icon-hover-zoom,
+        .eva-parent-hover:hover .eva-icon-hover-zoom {
+            animation-name: eva-zoomIn;
+        }
+
+        .eva-hover:hover .eva-icon-hover-pulse,
+        .eva-parent-hover:hover .eva-icon-hover-pulse {
+            animation-name: eva-pulse;
+        }
+
+        .eva-hover:hover .eva-icon-hover-flip,
+        .eva-parent-hover:hover .eva-icon-hover-flip {
+            animation-name: eva-flipInY;
+        }
+
+        @keyframes eva-flipInY {
+            from {
+                transform: perspective(400px) rotate3d(0, 1, 0, 90deg);
+                animation-timing-function: ease-in;
+                opacity: 0;
+            }
+
+            40% {
+                transform: perspective(400px) rotate3d(0, 1, 0, -20deg);
+                animation-timing-function: ease-in;
+            }
+
+            60% {
+                transform: perspective(400px) rotate3d(0, 1, 0, 10deg);
+                opacity: 1;
+            }
+
+            80% {
+                transform: perspective(400px) rotate3d(0, 1, 0, -5deg);
+            }
+
+            to {
+                transform: perspective(400px);
+            }
+        }
+
+        @keyframes eva-shake {
+
+            from,
+            to {
+                transform: translate3d(0, 0, 0);
+            }
+
+            10%,
+            30%,
+            50%,
+            70%,
+            90% {
+                transform: translate3d(-3px, 0, 0);
+            }
+
+            20%,
+            40%,
+            60%,
+            80% {
+                transform: translate3d(3px, 0, 0);
+            }
+        }
+
+        @keyframes eva-pulse {
+            from {
+                transform: scale3d(1, 1, 1);
+            }
+
+            50% {
+                transform: scale3d(1.2, 1.2, 1.2);
+            }
+
+            to {
+                transform: scale3d(1, 1, 1);
+            }
+        }
+
+        @keyframes eva-zoomIn {
+            from {
+                opacity: 1;
+                transform: scale3d(0.5, 0.5, 0.5);
+            }
+
+            50% {
+                opacity: 1;
+            }
+        }
+    </style>
+    <script src="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/ionicons.z18qlu2u.js"
+        data-resources-url="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/" data-namespace="ionicons">
+    </script>
+    <script src="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/ionicons.z18qlu2u.js"
+        data-resources-url="file:///C:/xampp/htdocs/html/assets/plugins/ionicons/ionicons/" data-namespace="ionicons">
+    </script>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+            width: 100%;
+            border: 1px solid #ddd;
+        }
+
+        th,
+        td {
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
 
 </head>
 
 <body class="main-body app sidebar-mini">
+
+    <!-- Loader -->
+    <div id="global-loader" style="display: none;">
+        <img src="assets/img/loader.svg" class="loader-img" alt="Loader">
+    </div>
+    <!-- /Loader -->
 
     <!-- Page -->
     <div class="page">
@@ -113,19 +328,17 @@
             <!-- main-header -->
             <div class="main-header sticky side-header nav nav-item" style="margin-bottom: -63px;">
                 <div class="container-fluid">
-
+                    <button type="button" class="btn btn-primary"
+                        style="position: absolute; left: 330;">เรียกพนักงานเข้ามารับพัสดุ</button>
+                    <button type="button" class="btn btn-primary"
+                        style="position: absolute; left: 540;">ระบุพนักงานเข้ารับพัสดุ</button>
 
                     <div class="main-header-left ">
                         <div class="app-sidebar__toggle" data-bs-toggle="sidebar">
                             <a class="open-toggle" href="#"><i class="header-icon fe fe-align-left"></i></a>
                             <a class="close-toggle" href="#"><i class="header-icons fe fe-x"></i></a>
                         </div>
-                        <button type="button" class="btn btn-primary mx-2">เรียกพนักงานเข้ามารับพัสดุ</button>
-                        <button type="button" class="btn btn-primary mx-2">ระบุพนักงานเข้ารับพัสดุ</button>
-
                     </div>
-
-
                     <div class="main-header-right">
                         <div class="nav nav-item  navbar-nav-right ml-auto">
                             <div class="nav-link" id="bs-example-navbar-collapse-1">
@@ -387,42 +600,13 @@
                                             <a class="text-muted">แล้ว</a></a>
                                     </p>
                                 </div>
-
-                                <div class="jumps-prevent d-flex justify-content-center" style="padding-top: 100px;">
-                                    <a type="button" data-toggle="modal" data-target="#exampleModalCenter"
-                                        style="color: red;">
-                                        <u>ยกเลิกรายการ</u>
-                                    </a>
-                                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-                                        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalCenterTitle">ต้องการยกเลิกรายการใช่หรือไม่
-                                                    </h5>
-                                                    <button type="button" class="close" data-dismiss="modal"
-                                                        aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    เหตุผลที่ทำการยกเลิก
-                                                    <input class="form-control" type="text" value="">
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <a href="cancel.html"  class="btn btn-danger">ยกเลิกรายการ</a>
-                                                    <button type="button" class="btn btn-outline-primary">เก็บไว้ก่อน</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
                                 <div class="jumps-prevent" style="padding-top: 25px;"></div>
 
                                 <div class="d-flex justify-content-center">
-                                    <a href="order.html" class="btn btn-danger mx-2" style="color: red;">ยกเลิก</a>
-                                    <a class="btn btn-primary mx-2" style="color: white;">สร้างรายการ</a>
+                                    <a class="btn btn-primary mx-2" href="remove v1.html" style="color: white;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M9.636 2.5a.5.5 0 0 0-.5-.5H2.5A1.5 1.5 0 0 0 1 3.5v10A1.5 1.5 0 0 0 2.5 15h10a1.5 1.5 0 0 0 1.5-1.5V6.864a.5.5 0 0 0-1 0V13.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                                        <path fill-rule="evenodd" d="M5 10.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1H6.707l8.147-8.146a.5.5 0 0 0-.708-.708L6 9.293V5.5a.5.5 0 0 0-1 0v5z"/>
+                                      </svg> แก้ไข</a>
                                 </div>
 
 
@@ -441,13 +625,25 @@
 
         <!-- Back-to-top -->
         <a href="#top" id="back-to-top" style="display: none;"><i class="las la-angle-double-up"></i></a>
+
+        <!-- JQuery min js -->
         <script src="assets/plugins/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Bundle js -->
         <script src="assets/plugins/bootstrap/js/popper.min.js"></script>
         <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Ionicons js -->
         <script src="assets/plugins/ionicons/ionicons.js"></script>
+
+        <!-- Moment js -->
         <script src="assets/plugins/moment/moment.js"></script>
+
+        <!-- P-scroll js -->
         <script src="assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
         <script src="assets/plugins/perfect-scrollbar/p-scroll.js"></script>
+
+        <!-- Internal Data tables -->
         <script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
         <script src="assets/plugins/datatable/datatables.min.js"></script>
         <script src="assets/plugins/datatable/js/dataTables.bootstrap5.js"></script>
@@ -459,16 +655,33 @@
         <script src="assets/plugins/datatable/js/buttons.colVis.min.js"></script>
         <script src="assets/plugins/datatable/pdfmake/pdfmake.min.js"></script>
         <script src="assets/plugins/datatable/pdfmake/vfs_fonts.js"></script>
+
+        <!--Internal  Datatable js -->
         <script src="assets/js/table-data.js"></script>
+
+        <!-- Rating js-->
         <script src="assets/plugins/rating/jquery.rating-stars.js"></script>
         <script src="assets/plugins/rating/jquery.barrating.js"></script>
+
+        <!-- Sidebar js -->
         <script src="assets/plugins/side-menu/sidemenu.js"></script>
+
+        <!-- Right-sidebar js -->
         <script src="assets/plugins/sidebar/sidebar.js"></script>
         <script src="assets/plugins/sidebar/sidebar-custom.js"></script>
+
+        <!-- Sticky js -->
         <script src="assets/js/sticky.js"></script>
+
+        <!-- eva-icons js -->
         <script src="assets/js/eva-icons.min.js"></script>
+
+        <!-- custom js -->
         <script src="assets/js/custom.js"></script>
 
+        <div class="main-navbar-backdrop"></div>
+        <div class="main-navbar-backdrop"></div>
+        <div class="main-navbar-backdrop"></div>
 </body>
 
 </html>
