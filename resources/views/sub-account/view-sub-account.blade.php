e82cd89b16f864e9f671585ebce719b11a68e4b3
Zudddege <tanapon085106@gmail.com>
2022-01-25 10:36:56 +0700

    table supaccount

--- resources/views/sub-account/view-sub-account.blade.php
+++ resources/views/sub-account/view-sub-account.blade.php
@@ -473,85 +473,17 @@
     <script src="{{asset('assets/js/eva-icons.min.js')}}"></script>
     <script src="{{asset('assets/js/custom.js')}}"></script>
 
-    <script>
-        $('#search').on("keyup", function () {
-            $('table.paginated').trigger('repaginate');
-        })
 
-        $('table.paginated').each(function () {
-            var currentPage = 0;
-            var numPerPage = 7;
-            var $table = $(this);
-            var $pager = $('<div class="pager"></div>');
-            var $previous = $('<span class="previous"><<</spnan>');
-            var $next = $('<span class="next">>></spnan>');
-
-            $pager.insertAfter($table).find('span.page-number:first').addClass('active');
-
-            $table.bind('repaginate', function () {
-                $table.find('tbody tr').hide();
-
-                $filteredRows = $table.find('tbody tr').filter(function (i, tr) {
-                    return $('#search').val() != "" ? $(tr).find("td").get().map(function (td) {
-                        return $(td).text();
-                    }).filter(function (td) {
-                        return td.indexOf($('#search').val()) != -1;
-                    }).length > 0 : true;
-                });
-
-                $filteredRows.slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-
-                var numRows = $filteredRows.length;
-                var numPages = Math.ceil(numRows / numPerPage);
-
-                $pager.find('.page-number, .previous, .next').remove();
-                for (var page = 0; page < numPages; page++) {
-                    var $newPage = $('<span class="page-number"></span>').text(page + 1).bind('click', {
-                        newPage: page
-                    }, function (event) {
-                        currentPage = event.data['newPage'];
-                        $table.trigger('repaginate');
-                    })
-                    if (page == currentPage) {
-                        $newPage.addClass('clickable active');
-                    } else {
-                        $newPage.addClass('clickable');
-                    }
-                    $newPage.appendTo($pager)
-                }
-
-                $previous.insertBefore('span.page-number:first');
-                $next.insertAfter('span.page-number:last');
-
-                $next.click(function (e) {
-                    $previous.addClass('clickable');
-                    $pager.find('.active').next('.page-number.clickable').click();
-                });
-                $previous.click(function (e) {
-                    $next.addClass('clickable');
-                    $pager.find('.active').prev('.page-number.clickable').click();
-                });
-
-                $next.addClass('clickable');
-                $previous.addClass('clickable');
-
-                setTimeout(function () {
-                    var $active = $pager.find('.page-number.active');
-                    if ($active.next('.page-number.clickable').length === 0) {
-                        $next.removeClass('clickable');
-                    } else if ($active.prev('.page-number.clickable').length === 0) {
-                        $previous.removeClass('clickable');
-                    }
-                });
-            });
-            $table.trigger('repaginate');
-        });
-
-    </script>
 
     <script>
         $('#my-table').DataTable({
             scrollX: false,
+            autoWidth: false,
+            searching: false,
+            filter: false,
+            ordering: false,
+            paging: false,
+            info: false,
             language: {
                 emptyTable: "ไม่พบข้อมูล"
             },
@@ -609,79 +541,80 @@
         });
 
     </script>
- <script>
-     $('#search').on("keyup", function() {
-        $('table.paginated').trigger('repaginate');
-        })
-
-        $('table.paginated').each(function() {
-        var currentPage = 0;
-        var numPerPage = 7;
-        var $table = $(this);
-        var $pager = $('<div class="pager"></div>');
-        var $previous = $('<span class="previous"><<</spnan>');
-        var $next = $('<span class="next">>></spnan>');
-
-        $pager.insertAfter($table).find('span.page-number:first').addClass('active');
-
-        $table.bind('repaginate', function() {
-            $table.find('tbody tr').hide();
-
-            $filteredRows = $table.find('tbody tr').filter(function(i, tr) {
-            return $('#search').val() != "" ? $(tr).find("td").get().map(function(td) {
-                return $(td).text();
-            }).filter(function(td){
-                return td.indexOf($('#search').val()) != -1;
-            }).length > 0 : true;
-            });
-
-            $filteredRows.slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-
-            var numRows = $filteredRows.length;
-            var numPages = Math.ceil(numRows / numPerPage);
-$pager.find('.page-number, .previous, .next').remove();
-            for (var page = 0; page < numPages; page++) {
-            var $newPage = $('<span class="page-number"></span>').text(page + 1).bind('click', {
-                newPage: page
-            }, function(event) {
-                currentPage = event.data['newPage'];
-                $table.trigger('repaginate');
+    <script>
+        $('#search').on("keyup", function() {
+            $('table.paginated').trigger('repaginate');
             })
-            if(page == currentPage){
-                $newPage.addClass('clickable active');
-                }else{
-                $newPage.addClass('clickable');
-            }
-            $newPage.appendTo($pager)
-            }
 
-            $previous.insertBefore('span.page-number:first');
-            $next.insertAfter('span.page-number:last');
+            $('table.paginated').each(function() {
+            var currentPage = 0;
+            var numPerPage = 7;
+            var $table = $(this);
+            var $pager = $('<div class="pager"></div>');
+            var $previous = $('<span class="previous"><<</spnan>');
+            var $next = $('<span class="next">>></spnan>');
 
-            $next.click(function(e) {
-            $previous.addClass('clickable');
-            $pager.find('.active').next('.page-number.clickable').click();
+            $pager.insertAfter($table).find('span.page-number:first').addClass('active');
+
+            $table.bind('repaginate', function() {
+                $table.find('tbody tr').hide();
+
+                $filteredRows = $table.find('tbody tr').filter(function(i, tr) {
+                return $('#search').val() != "" ? $(tr).find("td").get().map(function(td) {
+                    return $(td).text();
+                }).filter(function(td){
+                    return td.indexOf($('#search').val()) != -1;
+                }).length > 0 : true;
+                });
+
+                $filteredRows.slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+
+                var numRows = $filteredRows.length;
+                var numPages = Math.ceil(numRows / numPerPage);
+
+                $pager.find('.page-number, .previous, .next').remove();
+                for (var page = 0; page < numPages; page++) {
+                var $newPage = $('<span class="page-number"></span>').text(page + 1).bind('click', {
+                    newPage: page
+                }, function(event) {
+                    currentPage = event.data['newPage'];
+                    $table.trigger('repaginate');
+                })
+                if(page == currentPage){
+                    $newPage.addClass('clickable active');
+                    }else{
+                    $newPage.addClass('clickable');
+                }
+                $newPage.appendTo($pager)
+                }
+
+                $previous.insertBefore('span.page-number:first');
+                $next.insertAfter('span.page-number:last');
+
+                $next.click(function(e) {
+                $previous.addClass('clickable');
+                $pager.find('.active').next('.page-number.clickable').click();
+                });
+                $previous.click(function(e) {
+                $next.addClass('clickable');
+                $pager.find('.active').prev('.page-number.clickable').click();
+                });
+
+                $next.addClass('clickable');
+                $previous.addClass('clickable');
+
+                setTimeout(function() {
+                var $active = $pager.find('.page-number.active');
+                if ($active.next('.page-number.clickable').length === 0) {
+                    $next.removeClass('clickable');
+                } else if ($active.prev('.page-number.clickable').length === 0) {
+                    $previous.removeClass('clickable');
+                }
+                });
             });
-            $previous.click(function(e) {
-            $next.addClass('clickable');
-            $pager.find('.active').prev('.page-number.clickable').click();
+            $table.trigger('repaginate');
             });
-
-            $next.addClass('clickable');
-            $previous.addClass('clickable');
-
-            setTimeout(function() {
-            var $active = $pager.find('.page-number.active');
-            if ($active.next('.page-number.clickable').length === 0) {
-                $next.removeClass('clickable');
-            } else if ($active.prev('.page-number.clickable').length === 0) {
-                $previous.removeClass('clickable');
-            }
-            });
-        });
-        $table.trigger('repaginate');
-        });
- </script>
+    </script>
 
 </body>
 
