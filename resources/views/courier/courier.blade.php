1b7aac8c0c8212b60d26ff230f8af8d7280dff6e
Yuttasat <yuttasatpinjai@hotmail.com>
2022-03-04 11:19:01 +0700

    courier detail

--- resources/views/courier/courier.blade.php
+++ resources/views/courier/courier.blade.php
@@ -191,6 +191,7 @@
                                             <th>ที่อยู่เข้ารับพัสดุ</th>
                                             <th>ข้อมูลคูเรียร์</th>
                                             <th>หมายเหตุ</th>
+                                            <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -236,6 +237,7 @@
                                                 <div class="text-muted">{{$courier->operator_name}} ({{$courier->operator_id}})</div>
                                             </td>
                                             <td>{{$courier->note_detail}}</td>
+                                            <td class="td_detail shadow"><a href="{{url('/couriers/detail/'.$courier->id)}}" class="btn btn-link"><u>ดูรายละเอียด</u></a></td>
                                         </tr>
                                         @endforeach
                                     </tbody>
